9e8990b8573c3d2843906dc7c93bad21ebb9da5e
paulina <paulinafs89@gmail.com>
2019-09-12 16:54:12 -0300

    Tercer comit

---
+++ app/controllers/songs_controller.rb
@@ -0,0 +1,27 @@
+class SongsController < ApplicationController
+  def create
+    song = Song.new(song_params)
+    song.playlist_id = params[:playlist_id]
+
+    if song.save
+      redirect_to playlist_path(song.playlist), notice: 'Canción Guardado'
+    else
+      redirect_to playlist_path(song.playlist), notice: 'No se pudo guardar la canción'
+    end
+  end
+
+
+  def destroy
+  song = Song.find(params[:id])
+  playlist = song.playlist
+  song.destroy
+  redirect_to playlist_path(song.playlist), notice: 'Comentario Eliminado'
+  end
+
+
+    private
+    def song_params
+      params.require(:song).permit(:name, :artist)
+    end
+
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :playlists
+  resources :playlists do
+    post '/songs', to: 'songs#create'
+    delete '/songs/:id', to:'songs#destroy', as:'delete_song'
+  end
+
   resources :users
   root 'users#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
